9a32c963559c9d844ead1d73b6b8de95b2150d2b
Dylan Reimer <reimer.dylan@gmail.com>
2024-06-28 13:07:32 -0700

    Initial project import.

---
+++ Source/Flashlight/Aaron.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Aaron.h"
+
+// Sets default values
+AAaron::AAaron()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AAaron::BeginPlay()
+{
+	Radius = 500;
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAaron::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AAaron::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AAaron::Move(float AxisValue)
+{
+
+}
